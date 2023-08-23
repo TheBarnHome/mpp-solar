b6aeeb023a2d878fbda26ef6ee488abb05b31a06
DarkZeros <mailszeros@gmail.com>
2023-08-23 15:54:40 +0000

    Mod SerialIO to be faster

--- mppsolar/inout/serialio.py
+++ mppsolar/inout/serialio.py
@@ -9,26 +9,36 @@ log = logging.getLogger("SerialIO")
 
 
 class SerialIO(BaseIO):
+    serials = {}
     def __init__(self, *args, **kwargs) -> None:
         self._serial_port = get_kwargs(kwargs, "device_path")
-        self._serial_baud = get_kwargs(kwargs, "serial_baud")
+        self._serial_baud = get_kwargs(kwargs, "serial_baud")        
+
+    def reuse(self):
+        if (self._serial_port, self._serial_baud) not in SerialIO.serials:
+            SerialIO.serials[(self._serial_port, self._serial_baud)] = serial.serial_for_url(self._serial_port, self._serial_baud, timeout=1, write_timeout=1)
+        return SerialIO.serials[(self._serial_port, self._serial_baud)]
 
     def send_and_receive(self, *args, **kwargs) -> dict:
         full_command = get_kwargs(kwargs, "full_command")
         response_line = None
         log.debug(f"port {self._serial_port}, baudrate {self._serial_baud}")
         try:
-            with serial.serial_for_url(self._serial_port, self._serial_baud) as s:
+            start = time.time()
+            s = self.reuse()
+            #with serial.serial_for_url(self._serial_port, self._serial_baud, timeout=0.1, write_timeout=0.1) as s:
+            if True:
                 log.debug("Executing command via serialio...")
-                s.timeout = 1
-                s.write_timeout = 1
                 s.flushInput()
                 s.flushOutput()
                 s.write(full_command)
-                time.sleep(0.1)  # give serial port time to receive the data
+                #time.sleep(0.1)  # give serial port time to receive the data
                 response_line = s.read_until(b"\r")
+                #s.close()
                 log.debug("serial response was: %s", response_line)
-                return response_line
+            log.info("serial done")
+            log.debug(time.time() - start)
+            return response_line
         except Exception as e:
             log.warning(f"Serial read error: {e}")
         log.info("Command execution failed")
